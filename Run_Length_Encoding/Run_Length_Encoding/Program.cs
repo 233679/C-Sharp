598f43eec24c5b6ea827ee98f93a4bad4bcb35e5
Tye Crofton <233679@student.wqe.ac.uk>
2025-02-03 11:34:30 +0000

    Added Run_Length_Encoding for input string

---
+++ Run_Length_Encoding/Run_Length_Encoding/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Prompt user for input.
+        Console.Write("Enter input string\n: ");
+        string? input = Console.ReadLine();
+
+        // Exit early if null.
+        if (input == null)
+        {
+            return;
+        }
+
+        // Change type from nullable string to non-null string.
+        string text = input;
+        string output = "";
+
+        while (text.Length > 0)
+        {
+            // Get the letter and the amount of times it repeats from the start of the string.
+            char start_letter = text.ElementAt(0);
+            int repeating = text.TakeWhile(letter => letter == start_letter).ToArray().Length;
+            
+            // Combine that with the output buffer
+            output += repeating.ToString() + start_letter.ToString();
+
+            // Removed parsed chars from start of text.
+            text = text.Remove(0, repeating);
+        }
+
+        // Output the buffer
+        Console.WriteLine(output);
+    }
+}
